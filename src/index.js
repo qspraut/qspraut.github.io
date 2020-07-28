0024d45ea4e40b467c50c9ddb4fcded7a3a604a0
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2020-07-28 14:02:46 -0400

    finishing touches

--- src/index.js
+++ src/index.js
@@ -11,8 +11,14 @@ const About = (props) => {
 const Welcome = (props) => {
   return <div>Welcome</div>;
 };
+const Projects = (props) => {
+  return <div> If I had projects, this is where I would put them...</div>;
+};
 const Test = (props) => {
   return <div> ID: {props.match.params.id} </div>;
+};
+const ColorTest = (props) => {
+  return <div> You clicked on color: {props.match.params.color} </div>;
 };
 const FallBack = (props) => {
   return <div>URL Not Found</div>;
@@ -23,8 +29,11 @@ const Nav = (props) => {
       <ul>
         <li><NavLink to="/" exact>Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
         <li><NavLink to="/test/id1">test id1</NavLink></li>
         <li><NavLink to="/test/id2">test id2</NavLink></li>
+        <li><NavLink to="/color/purple">Purple</NavLink></li>
+        <li><NavLink to="/color/green">Green</NavLink></li>
       </ul>
     </nav>
   );
@@ -37,7 +46,9 @@ const App = (props) => {
         <Switch>
           <Route exact path="/" component={Welcome} />
           <Route path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
           <Route exact path="/test/:id" component={Test} />
+          <Route exact path="/color/:color" component={ColorTest} />
           <Route component={FallBack} />
         </Switch>
       </div>
