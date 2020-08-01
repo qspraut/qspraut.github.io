6d584a8c04564f7a4b8255a4fa1803ad3d0c9b2c
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2020-08-01 10:47:40 -0400

    SA6 changes

---
+++ src/components/app.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import '../style.scss';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
+import Counter from './counter';
+import Controls from './controls';
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return (
+    <div>
+      Welcome
+      <Counter />
+      <Controls />
+    </div>
+  );
+};
+const Projects = (props) => {
+  return <div> If I had projects, this is where I would put them...</div>;
+};
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+const ColorTest = (props) => {
+  return <div> You clicked on color: {props.match.params.color} </div>;
+};
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+        <li><NavLink to="/color/purple">Purple</NavLink></li>
+        <li><NavLink to="/color/green">Green</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route exact path="/color/:color" component={ColorTest} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
-import {
-  BrowserRouter as Router, Route, NavLink, Switch,
-} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-const About = (props) => {
-  return <div> All there is to know about me </div>;
-};
-const Welcome = (props) => {
-  return <div>Welcome</div>;
-};
-const Projects = (props) => {
-  return <div> If I had projects, this is where I would put them...</div>;
-};
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
-};
-const ColorTest = (props) => {
-  return <div> You clicked on color: {props.match.params.color} </div>;
-};
-const FallBack = (props) => {
-  return <div>URL Not Found</div>;
-};
-const Nav = (props) => {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/projects">Projects</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-        <li><NavLink to="/color/purple">Purple</NavLink></li>
-        <li><NavLink to="/color/green">Green</NavLink></li>
-      </ul>
-    </nav>
-  );
-};
-const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/test/:id" component={Test} />
-          <Route exact path="/color/:color" component={ColorTest} />
-          <Route component={FallBack} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+import App from './components/app';
+
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+// boilerplate to copy, don't have to know
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+));
+
+// we now wrap App in a Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('main'),
+);
